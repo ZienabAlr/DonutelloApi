602b07a9d337ba465aca373588bebbd732e0b7a1
ZienabAlr <r0784494@student.thomasmore.be>
2023-05-19 12:33:24 +0200

    feat(donutello.js): get request added

--- api/routes/api/v1/donutello.js
+++ api/routes/api/v1/donutello.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+
+    res.json({
+        "status": "success",
+        "data": {
+            "donuts": []    
+        }
+    });
+
+});
+
 router.post('/', (req, res) => {
 
     res.json({
