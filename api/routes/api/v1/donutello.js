d5704e669cbeb775ec6be1bea5e9e9b00084b701
ZienabAlr <r0784494@student.thomasmore.be>
2023-05-19 12:30:59 +0200

    feat(donutello.js): post request has been made

--- api/routes/api/v1/donutello.js
+++ api/routes/api/v1/donutello.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+router.post('/', (req, res) => {
 
+    res.json({
+        "status": "success",
+        "data": {
+            "title": "Donutello",
+            "description": "The Donut Lover's Donut",
+            "price": 1.99
+            // "img": "https://i.imgur.com/2yjgr2l.jpg"
+        }
+    });
+
+}); 
