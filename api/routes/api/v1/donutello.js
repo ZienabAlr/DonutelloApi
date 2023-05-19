dd3a3dff42ad1084d247a4df5c1df566068a16fe
ZienabAlr <r0784494@student.thomasmore.be>
2023-05-19 15:22:59 +0200

    feat(donutello.js): controller has been required in routes

--- api/routes/api/v1/donutello.js
+++ api/routes/api/v1/donutello.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const donutelloController = require('../../../controllers/api/v1/donutello');
 
 router.get('/', (req, res) => {
 
