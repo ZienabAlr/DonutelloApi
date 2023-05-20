308b8b6908bf8167618a397ebffe130d7ca76d0c
ZienabAlr <r0784494@student.thomasmore.be>
2023-05-20 16:39:40 +0200

    fix(app.js): connection to mongoose has been fixed

--- api/app.js
+++ api/app.js
@@ -9,7 +9,9 @@ const usersRouter = require('./routes/users');
 const apiDonutelloRouter  = require('./routes/api/v1/donutello');
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+mongoose.connect('mongodb://127.0.0.1:27017/test', {
+  useNewUrlParser: true,
+});
 
 const app = express();
 
