4f1003b6a3b4a415697dd649a10f245cbd61e596
ZienabAlr <r0784494@student.thomasmore.be>
2023-06-02 00:03:04 +0200

    feat(Donutello.js): models folder with Donutello.js added and mongoose schema has been pasted in Donutello.js

---
+++ api/models/Donutello.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const donutSchema = new Schema({
+    company: String,
+    email: String,
+    phone: String,
+    donutname: String,
+    glaze: String,
+    amount: Number,
+    notes: String
+
+})
+const Donut = mongoose.model('Donut', donutSchema);
